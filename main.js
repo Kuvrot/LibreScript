7c502a013a20de86662d0595f988b400b58218d8
Kuvrot <jamessanz@outlook.com>
2024-06-03 18:54:09 -0600

    added markdown like language

--- main.js
+++ main.js
@@ -15,8 +15,27 @@ window.addEventListener("DOMContentLoaded", function () {
     applyPageBreaks();
     setFormat(3);
     setTime();
+    updateDivContent();
 });
 
+//Syntax
+var syntax = {
+    "-":"<p style='padding-left:10.4cm;'>",
+   "_":"<p style='padding-left:6.8cm; padding-top:0.1%;'>",
+   "=": "<p style='padding-left:4.3cm;'>",
+   "\n": "<br>"
+};
+
+var docContent; 
+
+function updateDivContent() {
+    docContent = document.querySelector('textarea').value;
+    var t = docContent.replace(/-|_|=|\n/gi, function(matched){
+        return syntax[matched];
+      });
+    document.getElementById('result').innerHTML = t;
+
+}
 
 function setTime () {
   
@@ -76,91 +95,10 @@ function setFormat (index) {
     editor.focus();
 }
 
-function applyPageBreaks() {
-    applyManualPageBreaks();
-    applyAutomaticPageBreaks(Config.pixelsPerInch, Config.pageHeightInCentimeter, Config.pageMarginBottomInCentimeter);
+function exportPDF () {
 
-    document.querySelectorAll(".document .page").forEach(function (element) {
-        if (!element.classList.contains("has-events")) {
-            element.addEventListener("blur", function () {
-                applyPageBreaks();
-            });
+    var content = document.getElementById("result").innerHTML;
+    var result = window.open('');
+    result.document.write('<head><title>LibreScript</title><link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous"><link href="css/sheet.css" rel="stylesheet"></head><body><div>'+content+'</div><script>window.print();</script><body/>');
 
-            element.classList.add("has-events");
-        }
-    });
-}
-
-function exportScript (){
-
-    var content = document.getElementById('document').innerHTML;
-    var doc = window.open('');
-    doc.document.write('<!DOCTYPE html> <html><head><title>Preview</title> <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">'
-    + '<link rel="stylesheet" type="text/css" href="css/sheets-of-paper-a4.css"> <link rel="stylesheet" href="css/style.css"></head><body><div class="document">' + content + '</document> </body></html>');
-    
-      setTimeout(function() {
-        doc.print();
-        doc.close();
-      }, 350);
-  
-
-}
-
-/* Applies any manual page breaks in preview mode (screen, non-print) where CSS Paged Media is not fully supported */
-function applyManualPageBreaks() {
-    var docs, pages, snippets;
-    docs = document.querySelectorAll(".document");
-
-    for (var d = docs.length - 1; d >= 0; d--) {
-        pages = docs[d].querySelectorAll(".page");
-
-        for (var p = pages.length - 1; p >= 0; p--) {
-            snippets = pages[p].children;
-
-            for (var s = snippets.length - 1; s >= 0; s--) {
-                if (snippets[s].classList.contains("page-break")) {
-                    pages[p].insertAdjacentHTML("afterend", "<div class=\"page\" contenteditable=\"true\"></div>");
-
-                    for (var n = snippets.length - 1; n > s; n--) {
-                        pages[p].nextElementSibling.insertBefore(snippets[n], pages[p].nextElementSibling.firstChild);
-                    }
-
-                    snippets[s].remove();
-                }
-            }
-        }
-    }
-}
-
-/* Applies (where necessary) automatic page breaks in preview mode (screen, non-print) where CSS Paged Media is not fully supported */
-function applyAutomaticPageBreaks(pixelsPerInch, pageHeightInCentimeter, pageMarginBottomInCentimeter) {
-    var inchPerCentimeter = 0.393701;
-    var pageHeightInInch = pageHeightInCentimeter * inchPerCentimeter;
-    var pageHeightInPixels = Math.ceil(pageHeightInInch * pixelsPerInch);
-    var pageMarginBottomInInch = pageMarginBottomInCentimeter * inchPerCentimeter;
-    var pageMarginBottomInPixels = Math.ceil(pageMarginBottomInInch * pixelsPerInch);
-    var docs, pages, snippets, pageCoords, snippetCoords;
-    docs = document.querySelectorAll(".document");
-
-    for (var d = docs.length - 1; d >= 0; d--) {
-        pages = docs[d].querySelectorAll(".page");
-
-        for (var p = 0; p < pages.length; p++) {
-            if (pages[p].clientHeight > pageHeightInPixels) {
-                pages[p].insertAdjacentHTML("afterend", "<div class=\"page\" contenteditable=\"true\"></div>");
-                pageCoords = pages[p].getBoundingClientRect();
-                snippets = pages[p].querySelectorAll("h1, h2, h3, h4, h5, h6, p, ul, ol");
-
-                for (var s = snippets.length - 1; s >= 0; s--) {
-                    snippetCoords = snippets[s].getBoundingClientRect();
-
-                    if ((snippetCoords.bottom - pageCoords.top + pageMarginBottomInPixels) > pageHeightInPixels) {
-                        pages[p].nextElementSibling.insertBefore(snippets[s], pages[p].nextElementSibling.firstChild);
-                    }
-                }
-
-                pages = docs[d].querySelectorAll(".page");
-            }
-        }
-    }
 }
